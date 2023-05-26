c39b3a32eae3c5f9ca144305c70ff0ac2eabaeeb
mktcommunity <104900670+mktcommunity@users.noreply.github.com>
2023-05-26 13:34:28 +0100

    push

--- scripts/upload-nfts.mjs
+++ scripts/upload-nfts.mjs
@@ -6,20 +6,27 @@ config();
 const sdk = ThirdwebSDK.fromPrivateKey(process.env.PRIVATE_KEY, "mumbai");
 
 const addNFTs = async () => {
-  const drop = await sdk.getContract(
-    "0x40739224a8818C381acaeAe2c620eeD84fEd35F2",
-    "nft-drop"
-  );
+	const drop = await sdk.getContract(
+		"0xebCa9017DD10078a3fa42CDBeb735d6De8276c22",
+		"nft-drop"
+	);
 
-  try {
-    await drop.createBatch([
-      "https://my-json-server.typicode.com/thirdweb-dev/updatable-nft-metadata/nfts/0",
-      "https://my-json-server.typicode.com/thirdweb-dev/updatable-nft-metadata/nfts/1",
-    ]);
-    console.log("uploaded all nfts");
-  } catch (error) {
-    console.log(error);
-  }
+	const urlBase =
+		"https://my-json-server.typicode.com/community0x/updatable-nft-metadata/nfts/";
+	const numberOfNFTs = 10; // Change this value to the actual number of NFTs
+
+	let urls = [];
+	//change i=0 and remove +1
+	for (let i = 1; i < numberOfNFTs + 1; i++) {
+		urls.push(urlBase + i);
+	}
+	console.log(urls);
+	try {
+		await drop.createBatch(urls);
+		console.log("uploaded all nfts");
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 addNFTs();
