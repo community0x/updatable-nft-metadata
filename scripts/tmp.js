112539dece4bdea9ecf3ab3cd3ecfc9bcf6c2ae7
mktcommunity <104900670+mktcommunity@users.noreply.github.com>
2023-05-29 13:30:10 +0100

    c

---
+++ scripts/tmp.js
@@ -0,0 +1,3 @@
+0xebCa9017DD10078a3fa42CDBeb735d6De8276c22
+
+https://my-json-server.typicode.com/community0x/updatable-nft-metadata/nfts/
--- scripts/upload-nfts.mjs
+++ scripts/upload-nfts.mjs
@@ -13,16 +13,16 @@ const addNFTs = async () => {
 
 	const urlBase =
 		"https://my-json-server.typicode.com/community0x/updatable-nft-metadata/nfts/";
-	const numberOfNFTs = 10; // Change this value to the actual number of NFTs
+	const numberOfNFTs = 11; // Change this value to the actual number of NFTs
 
 	let urls = [];
 	//change i=0 and remove +1
-	for (let i = 1; i < numberOfNFTs + 1; i++) {
+	for (let i = 1; i < numberOfNFTs; i++) {
 		urls.push(urlBase + i);
 	}
 	console.log(urls);
 	try {
-		await drop.createBatch(urls);
+		//await drop.createBatch(urls);
 		console.log("uploaded all nfts");
 	} catch (error) {
 		console.log(error);
